560df2fd9d48cf768aac86d03b5d817985a4cd54
Michael Lauderback <mlauderb@iastate.edu>
2017-07-11 22:11:49 -0500

    Added comments for clarity - checklm

--- src/boot/checklm.asm
+++ src/boot/checklm.asm
@@ -1,33 +1,35 @@
 checklm:
 pusha
 
-pushfd
-pop eax
-mov ecx, eax
+pushfd            ; push EFLAGS to the stack
+pop eax           ; pop EFLAGS from stack to EAX
+mov ecx, eax      ; back up EAX in ECX
 
-xor eax, 1 << 21
+xor eax, 1 << 21  ; FLIP 21st bit of EAX to 1 if it isn't already
 
-push eax
+push eax          ; lines 10-14 put EAX back into EFLAGS and then back into EAX
 popfd
 
 pushfd
 pop eax
 
-xor eax, ecx
-jz .done
+xor eax, ecx      ; return true (1) if EAX and original EFLAGS (backed up in ECX) are different
+jz .done          ; if 0, jump to .done
 
+; test if cpuid supports extended processor info
 mov eax, 0x80000000
 cpuid
-cmp eax, 0x80000001
-jb .done
+cmp eax, 0x80000001 ; eax must be larger than or equal to 0x80000001
+jb .done            ; if it is not, jump to .done
 
+; test if CPU supports long mode
 mov eax, 0x80000001
 cpuid
-test edx, 1 << 29
-jz .done
+test edx, 1 << 29 ; if the 29th bit of edx is 1, we have long mode
+jz .done          ; if not, jump to .done
 
 mov si, YES_LM
-call printf
+call printf       ; print success feedback
 popa
 ret
 
@@ -35,4 +37,4 @@ ret
 popa
 mov si, NO_LM
 call printf
-jmp $
+jmp $             ; hang after printing error message
